91074fac6c7294958b6aa4626cfca4369679bb9e
TBM97 <98299315+TBM97@users.noreply.github.com>
2022-03-22 18:22:17 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 // var APIKey = "AIzaSyAIfrYqV42vZikjEowH8Lh4CtsgCpKMQXI";
 //var APIKey = "AIzaSyAcAK8zAbrh0XiEyVmDFtrqIEnY7N4Qrag";
+var APIKey ="AIzaSyB4rCoMIg699n89bxSQSP47jsdkpRgXRJ4"
 var userArrayArtist = JSON.parse(localStorage.getItem("Last Artist")) || [];
 var userArraySong = JSON.parse(localStorage.getItem("Last Song")) || [];
 var video = "";
-$(document).ready(function(){
-
+$(document).ready(function () {
 
 });
-
 // This is what happens when you click the search button
 
-function newSearch () {
+function newSearch() {
     var userInputArtist = $("#searchArtist").val().trim();
     var userInputSong = $("#searchSong").val().trim();
     videoSearch(APIKey, userInputArtist + "+" + userInputSong, 3);
@@ -24,41 +23,41 @@ function newSearch () {
 
 // Function for searching videos using Youtube API
 
-function videoSearch (key, search, maxResults) {
-    $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data){
+function videoSearch(key, search, maxResults) {
+    $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
         console.log(data.items);
-        data.items.forEach(function(item, index) {
-            video = `
-            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
-             `
-            $("#video" + (index + 1)).html(video);
-            
-        })
+        data.items.forEach(function (item, index) {
+           video = `
+           <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            `
+           $("#video" + (index + 1)).html(video);
+
+       })
     })
 };
 
 // Function for searching lyrics using Lyrics OVH
 
-function lyricSearch (artist, song) {
+function lyricSearch(artist, song) {
     var artist = $("#searchArtist").val().trim().replace(/ /g, '+');
     var song = $("#searchSong").val().trim().replace(/ /g, '+');
 
     $.get("https://api.lyrics.ovh/v1/" + artist + "/" + song, function (data) {
         $("#lyricsDisplay").html(data.lyrics.replace(new RegExp("\n", "g"), "<br>"));
-        
+
         localStorage.setItem("Saved Lyrics", JSON.stringify(data));
     })
 };
 
 // Function for storing the artist name in local storage
 
-function storeDataArtist () {
+function storeDataArtist() {
     var userInputArtist = $("#searchArtist").val().trim().replace(/ /g, '+');
     var containsSearch = false;
 
     if (userArrayArtist != null) {
-        
-        $(userArrayArtist).each(function(x) {
+
+        $(userArrayArtist).each(function (x) {
             if (userArrayArtist[x] === userInputArtist) {
                 // containsSearch = true;
             }
@@ -76,13 +75,13 @@ function storeDataArtist () {
 
 // Function for storing the song name in local storage
 
-function storeDataSong () {
+function storeDataSong() {
     var userInputSong = $("#searchSong").val().trim().replace(/ /g, '+');
     var containsSearch = false;
 
     if (userArraySong != null) {
-        
-        $(userArraySong).each(function(x) {
+
+        $(userArraySong).each(function (x) {
             if (userArraySong[x] === userInputSong) {
                 // containsSearch = true;
             }
@@ -99,23 +98,23 @@ function storeDataSong () {
 
 // This hides the carousel, if the user wants to hide it, but he doesn't have to... personal choice
 
-    $("#hide").click(function(){
-      $(".orbit-container").hide();
-      $(".orbit-bullets").hide();
-      $(".orbit-previous").hide();
-      $(".orbit-next").hide();
-    });
+$("#hide").click(function () {
+    $(".orbit-container").hide();
+    $(".orbit-bullets").hide();
+    $(".orbit-previous").hide();
+    $(".orbit-next").hide();
+});
 
-    $("#show").click(function(){
-      $(".orbit-container").show();
-      $(".orbit-bullets").show();
-      $(".orbit-previous").show();
-      $(".orbit-next").show();
-    });
+$("#show").click(function () {
+    $(".orbit-container").show();
+    $(".orbit-bullets").show();
+    $(".orbit-previous").show();
+    $(".orbit-next").show();
+});
 
 // Event listener for the search button
 
-$(".theButton").on("click", function(event){
+$(".theButton").on("click", function (event) {
     event.preventDefault();
     newSearch();
     $('ul li:first-child').addClass("is-active");
@@ -124,37 +123,34 @@ $(".theButton").on("click", function(event){
 
 // Event listener for Enter Key
 
-$(".input artist-input").on("keyup", function(event){
+$(".input artist-input").on("keyup", function (event) {
     if (event.keyCode === 13) {
         newSearch();
-        $('.videos').css("display","block");
+        $('.videos').css("display", "block");
         $('ul li:first-child').addClass("is-active");
-    
+
     }
 });
-$(".input lyric-input").on("keyup", function(event){
+$(".input lyric-input").on("keyup", function (event) {
     if (event.keyCode === 13) {
         newSearch();
-        $('.videos').css("display","block");
+        $('.videos').css("display", "block");
         $('ul li:first-child').addClass("is-active");
-    
+
     }
 });
 
 // This makes the karaoke button change the background to a more 'fun' animated video
 
-$(".karaoke").on("click", function(){
+$(".karaoke").on("click", function () {
     var background = $("#bgvid");
     var body = $("#app-body")
     if (background.css("display") === "none") {
         background.css("display", "block");
         body.css("color", "white");
     } else {
-      background.css("display","none");
-      body.css("color","black");
+        background.css("display", "none");
+        body.css("color", "black");
 
     }
 });
-
-
-
